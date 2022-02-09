4297faa2737bc164176bc5f6b64974916400f962
gimaidar <aidar_008@bk.ru>
2022-02-09 21:32:40 +0500

    подключил логгирование

--- src/main/java/com/gimaletdinov/dictionary/dictionary_application_mvc/aspect/LoggingAspect.java
+++ src/main/java/com/gimaletdinov/dictionary/dictionary_application_mvc/aspect/LoggingAspect.java
@@ -4,20 +4,25 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Around("execution(* com.gimaletdinov.dictionary.dictionary_application_mvc.dao.*.*(..))")
     public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
         MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         String methodName = methodSignature.getName();
-        System.out.println("Begin of " + methodName);
+
+        LOGGER.info("Begin of " + methodName);
 
         Object targetMethodResult = proceedingJoinPoint.proceed(); //выполняем работу таргет метода
 
-        System.out.println("End of " + methodName);
+        LOGGER.info("End of " + methodName);
 
         return targetMethodResult;
     }
